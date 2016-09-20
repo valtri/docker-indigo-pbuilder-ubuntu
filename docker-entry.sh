1f0622d805f783e09cf8c0651b37f33c694bd463
František Dvořák <valtri@civ.zcu.cz>
2016-09-20 23:19:54 +0200

    Fix initial base image creation (su usage).

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if ! test -f /var/cache/pbuilder/ubuntu-14-x86_64.tgz; then
-    su jenkins -c ~jenkins/scripts/refresh-chroot --initial || :
+    su jenkins -c "~jenkins/scripts/refresh-chroot --initial" || :
 fi
 
 exec "$@"
