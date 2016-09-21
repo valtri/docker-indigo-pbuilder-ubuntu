4c0eb883926de02ca500d4e5c5b9619b0f04e734
František Dvořák <valtri@civ.zcu.cz>
2016-09-21 12:45:42 +0200

    Sync repository configuration with docker-indigo-mock-centos image.

--- site.pp
+++ site.pp
@@ -27,6 +27,38 @@ file{ 'G00precedence':
 
 file{ 'repos.sh':
   content => "case \$PLATFORM in
+        epel*|fedora*)
+                cat <<EOF
+[INDIGO-${version}-base]
+name=INDIGO-${version} - Base
+baseurl=http://repo.indigo-datacloud.eu/repository/${repo}/${version}/centos7/\\\$basearch/base
+protect=1
+enabled=1
+# To use priorities you must have yum-priorities installed
+priority=40
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-indigodc
+
+#[INDIGO-${version}-updates]
+#name=INDIGO-${version} - Updates
+#baseurl=http://repo.indigo-datacloud.eu/repository/${repo}/${version}/centos7/\\\$basearch/updates
+#protect=1
+#enabled=1
+## To use priorities you must have yum-priorities installed
+#priority=40
+
+[INDIGO-${version}-third-party]
+name=INDIGO-${version} - Third party
+baseurl=http://repo.indigo-datacloud.eu/repository/${repo}/${version}/centos7/\\\$basearch/third-party
+protect=1
+enabled=1
+# To use priorities you must have yum-priorities installed
+priority=40
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-indigodc
+EOF
+        ;;
+
         debian*|ubuntu*)
                 cat <<EOF
 #### INDIGO-${version} - Base repository - must be enabled ####
